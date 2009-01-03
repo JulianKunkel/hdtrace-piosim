e7d2c9c737310ea980b3be67a05076314dced606
Julian Kunkel <kunkel@dkrz.de>
2009-01-03 19:06:19 +0000

    Fixed classpath.

--- PIOsimHD-Simulator/maint/create-javadoc-with-umlgraph.sh
+++ PIOsimHD-Simulator/maint/create-javadoc-with-umlgraph.sh
@@ -2,5 +2,11 @@
 
 mkdir javadoc
 cd javadoc
+
+CP="-classpath ../../lib/TAU_tf.jar:../../../PIOsimHD-Model/lib/junit-4.5.jar"
+
+#create std. javadoc
+#javadoc -d . -private $CP -sourcepath ../../../PIOsimHD-Simulator/src/:../../../PIOsimHD-Model/src/ -subpackages de.hd
+
 # automatically generate javadoc:
-javadoc javadoc  -doclet org.umlgraph.doclet.UmlGraphDoc -private -docletpath ../umlgraph/UmlGraph.jar  -sourcepath ../../../PIOsimHD-Simulator/src/:../../../PIOsimHD-Model/src/ -subpackages de.hd -inferrel -inferdep -hide java.* -collpackages "java.util.*" -qualify -postfixpackage -nodefontsize 9 -nodefontpackagesize 7
+javadoc -d "."  -doclet org.umlgraph.doclet.UmlGraphDoc -private $CP -docletpath ../umlgraph/UmlGraph.jar  -sourcepath ../../../PIOsimHD-Simulator/src/:../../../PIOsimHD-Model/src/ -subpackages de.hd -inferrel -inferdep -hide java.* -collpackages "java.util.*" -qualify -postfixpackage -nodefontsize 9 -nodefontpackagesize 7
