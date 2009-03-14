c806f497125ef87bdb4a864578ef59500463fb36
Julian Kunkel <kunkel@dkrz.de>
2009-03-14 11:13:05 +0000

    Refactored StatisticType.

---
+++ PIOsimHD-Maint/TraceConverterHD/src/de/hd/pvs/traceConverter/Input/Statistics/StatisticType.java
@@ -0,0 +1,15 @@
+package de.hd.pvs.traceConverter.Input.Statistics;
+
+/**
+ * Data types a statistic value might have.
+ * @author julian
+ *
+ */
+public enum StatisticType{
+	FLOAT,
+	INT,
+	LONG,
+	DOUBLE,
+	STRING,
+	EPOCH
+}
