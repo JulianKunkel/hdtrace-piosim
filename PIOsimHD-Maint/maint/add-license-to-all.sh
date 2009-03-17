341e88f4f428eaf06a8b8f05510a5cd18c520076
Julian Kunkel <kunkel@dkrz.de>
2009-03-17 10:58:12 +0000

    Moved TraceConverterHD to Top-level project.

--- PIOsimHD-Maint/maint/add-license-to-all.sh
+++ PIOsimHD-Maint/maint/add-license-to-all.sh
@@ -4,7 +4,7 @@ source `dirname $0`/../path.rc || exit 1
 
 cd `dirname $0`
 
-for FOLDER in $SIMF $MODELF $MAINTF/TraceConverterHD $TRACEF; do
+for FOLDER in $SIMF $MODELF $TRACECONVERTERF $TRACEF; do
 	./add-license.sh $FOLDER/src
 done
   
