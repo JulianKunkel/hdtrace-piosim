d0a0358c43ffa830cdf2b09cda237432122a1c03
Tien Duc Dinh <cau.n.kau@gmail.com>
2009-07-21 19:34:32 +0000

    add pint-event-hd-client.h to repository

--- PVFS2/2.8.1/src/common/misc/pint-event-hd-client.h
+++ PVFS2/2.8.1/src/common/misc/pint-event-hd-client.h
@@ -1,4 +1,3 @@
-
 #ifndef __PINT_EVENT_CLIENT_HD_H
 #define __PINT_EVENT_CLIENT_HD_H
 
@@ -7,3 +6,4 @@
 int PVFS_HD_client_trace_initialize(hdTopology * topo, hdTopoNode * parentNode);
 
 #endif /* __PINT_EVENT_CLIENT_HD_H */
+
