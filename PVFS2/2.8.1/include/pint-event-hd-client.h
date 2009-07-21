fecc48ac6e5b3f8d0ec18552a0596dc73da9ad33
Tien Duc Dinh <cau.n.kau@gmail.com>
2009-07-21 19:20:03 +0000

    use * in fkt

--- PVFS2/2.8.1/include/pint-event-hd-client.h
+++ PVFS2/2.8.1/include/pint-event-hd-client.h
@@ -4,6 +4,6 @@
 
 #include "hdTopo.h"
 
-int PVFS_HD_client_trace_initialize(hdTopology topo, hdTopoNode parentNode);
+int PVFS_HD_client_trace_initialize(hdTopology * topo, hdTopoNode * parentNode);
 
 #endif /* __PINT_EVENT_CLIENT_HD_H */
