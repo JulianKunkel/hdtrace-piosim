745468e11de48ea8c7bdef1e561491badecab12f
Michael Kuhn <michael.kuhn@informatik.uni-hamburg.de>
2009-07-25 03:03:36 +0000

    Add comments.

--- PIOsimHD-Simulator/src/de/hd/pvs/piosim/simulator/components/ServerCacheLayer/GServerDirectedIO.java
+++ PIOsimHD-Simulator/src/de/hd/pvs/piosim/simulator/components/ServerCacheLayer/GServerDirectedIO.java
@@ -1,27 +1,27 @@
 /** Version Control Information $Id$
  * @lastmodified    $Date$
  * @modifiedby      $LastChangedBy$
- * @version         $Revision$ 
+ * @version         $Revision$
  */
 
 //	Copyright (C) 2009 Michael Kuhn
-//	
+//
 //	This file is part of PIOsimHD.
-//	
+//
 //	PIOsimHD is free software: you can redistribute it and/or modify
 //	it under the terms of the GNU General Public License as published by
 //	the Free Software Foundation, either version 3 of the License, or
 //	(at your option) any later version.
-//	
+//
 //	PIOsimHD is distributed in the hope that it will be useful,
 //	but WITHOUT ANY WARRANTY; without even the implied warranty of
 //	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //	GNU General Public License for more details.
-//	
+//
 //	You should have received a copy of the GNU General Public License
 //	along with PIOsimHD.  If not, see <http://www.gnu.org/licenses/>.
 /**
- * 
+ *
  */
 package de.hd.pvs.piosim.simulator.components.ServerCacheLayer;
 
@@ -40,7 +40,7 @@ import de.hd.pvs.piosim.simulator.network.jobs.requests.RequestRead;
 
 /**
  * Try to do clever I/O optimization.
- * 
+ *
  * @author Michael Kuhn
  */
 public class GServerDirectedIO extends GAggregationCache {
@@ -56,21 +56,23 @@ public class GServerDirectedIO extends GAggregationCache {
 		}
 	}
 
+	/* Remember the last file and offset to perform contiguous operations,
+	 * when possible. */
 	MPIFile lastFile = null;
 	long lastOffset = -1;
 
+	/* Use per-file queues to make merging easier. */
 	HashMap<MPIFile, LinkedList<IOJob>> queuedReadJobs = new HashMap<MPIFile, LinkedList<IOJob>>();
 	HashMap<MPIFile, LinkedList<IOJob>> queuedWriteJobs = new HashMap<MPIFile, LinkedList<IOJob>>();
 
+	/* Merge src HashMap into dst HashMap. */
 	private void mergeHashMaps(HashMap<IOJob, RequestRead> dst, HashMap<IOJob, RequestRead> src) {
 		for (IOJob io : src.keySet()) {
 			dst.put(io, src.get(io));
 		}
 	}
 
-	/*
-	 * Combine as many IOJobs as possible.
-	 */
+	/* Combine as many IOJobs as possible. */
 	private LinkedList<IOJob> mergeReadJobs(LinkedList<IOJob> l) {
 		long size = -1;
 		long offset = -1;
@@ -83,14 +85,21 @@ public class GServerDirectedIO extends GAggregationCache {
 
 		System.out.print("MERGE READ old: " + l.size());
 
-		// FIXME
+		/* FIXME: Sort job queue. Real implementations should probably keep the list sorted on insert. */
 		Collections.sort(l, new IOJobComparator());
 
 		it = l.iterator();
 
+		/* This loop merges IOJobs by iterating over the job queue and removing
+		 * IOJobs that can be merged. When two IOJobs are too far apart or
+		 * the merged IOJob would be larger than IOGranularity, the old (merged)
+		 * IOJob is inserted into a new job queue (the old one can not be modified
+		 * without invalidating the iterator) and a new merge is started.
+		 * Special care must be taken with regards to pendingReadRequestMap. */
 		while (it.hasNext()) {
 			IOJob cur = it.next();
 
+			/* FIXME: Determine file. */
 			if (file == null) {
 				file = cur.getFile();
 			}
@@ -106,6 +115,7 @@ public class GServerDirectedIO extends GAggregationCache {
 			}
 
 			if (offset + size + (getSimulator().getModel().getGlobalSettings().getIOGranularity() / 10) >= cur.getOffset()) {
+				/* Merge two IOJobs. We allow holes of size (IOGranularity / 10). */
 				long newOffset;
 				long newSize;
 
@@ -113,6 +123,7 @@ public class GServerDirectedIO extends GAggregationCache {
 				newSize = Math.max(offset + size, cur.getOffset() + cur.getSize()) - newOffset;
 
 				if (newSize > getSimulator().getModel().getGlobalSettings().getIOGranularity()) {
+					/* New IOJob would get too large. Start a new one. */
 					IOJob tmp;
 
 					tmp = new IOJob(io.getFile(), size, offset, io.getType());
@@ -139,6 +150,7 @@ public class GServerDirectedIO extends GAggregationCache {
 				mergeHashMaps(map, pendingReadRequestMap.remove(cur));
 				it.remove();
 			} else {
+				/* IOJobs can not be merged. Start a new one. */
 				IOJob tmp;
 
 				tmp = new IOJob(io.getFile(), size, offset, io.getType());
@@ -196,9 +208,15 @@ public class GServerDirectedIO extends GAggregationCache {
 
 		it = l.iterator();
 
+		/* This loop merges IOJobs by iterating over the job queue and removing
+		 * IOJobs that can be merged. When two IOJobs are too far apart or
+		 * the merged IOJob would be larger than IOGranularity, the old (merged)
+		 * IOJob is inserted into a new job queue (the old one can not be modified
+		 * without invalidating the iterator) and a new merge is started. */
 		while (it.hasNext()) {
 			IOJob cur = it.next();
 
+			/* FIXME: Determine file. */
 			if (file == null) {
 				file = cur.getFile();
 			}
@@ -212,8 +230,9 @@ public class GServerDirectedIO extends GAggregationCache {
 			}
 
 			if (offset + size == cur.getOffset()) {
-				// forward combination
+				/* Merge two IOJobs. */
 				if (size + cur.getSize() > getSimulator().getModel().getGlobalSettings().getIOGranularity()) {
+					/* New IOJob would get too large. Start a new one. */
 					nl.add(new IOJob(io.getFile(), size, offset, io.getType()));
 
 					io = cur;
@@ -226,6 +245,7 @@ public class GServerDirectedIO extends GAggregationCache {
 				size += cur.getSize();
 				it.remove();
 			} else {
+				/* IOJobs can not be merged. Start a new one. */
 				nl.add(new IOJob(io.getFile(), size, offset, io.getType()));
 
 				io = cur;
@@ -275,19 +295,23 @@ public class GServerDirectedIO extends GAggregationCache {
 			lastFileList = queue.get(lastFile);
 		}
 
+		/* Take a look at all job queues. */
 		for (LinkedList<IOJob> l : queue.values()) {
 			long tmp = 0;
 
 			it = l.iterator();
 
+			/* Determine the size of the job queue. */
 			while (it.hasNext()) {
 				tmp += it.next().getSize();
 			}
 
+			/* Prefer operating on the last file. */
 			if (l == lastFileList) {
 				tmp *= 10;
 			}
 
+			/* Use the largest job queue. */
 			if (tmp > size) {
 				size = tmp;
 				list = l;
@@ -311,15 +335,19 @@ public class GServerDirectedIO extends GAggregationCache {
 
 		it = list.iterator();
 
+		/* Take a look at all IOJobs. */
 		while (it.hasNext()) {
 			IOJob cur = it.next();
 
 			if (lastOffset >= 0 && cur.getOffset() == lastOffset) {
+				/* We can resume writing at the last position, perfect match. */
 				io = cur;
 				break;
 			} else if (ioClose == null || Math.abs(cur.getOffset() - lastOffset) < Math.abs(ioClose.getOffset() - lastOffset)) {
+				/* Determine the IOJob closest to the last position. */
 				ioClose = cur;
 			} else if (ioLarge == null || cur.getSize() > ioLarge.getSize()) {
+				/* Determine the largest IOJob. */
 				ioLarge = cur;
 			}
 		}
@@ -330,12 +358,15 @@ public class GServerDirectedIO extends GAggregationCache {
 			} else if (ioClose == null && ioLarge != null) {
 				io = ioLarge;
 			} else if (ioClose != null && ioLarge != null) {
+				/* Use the largest IOJob if it is at least ten times bigger
+				 * then the closest one. */
 				if (ioLarge.getSize() > ioClose.getSize() * 10) {
 					io = ioLarge;
 				} else {
 					io = ioClose;
 				}
 			} else {
+				/* Just use the first IOJob. */
 				io = list.peek();
 			}
 		}
@@ -364,6 +395,7 @@ public class GServerDirectedIO extends GAggregationCache {
 	protected IOJob getNextSchedulableJob() {
 		IOJob io;
 
+		/* We prefer read jobs. */
 		io = getJob(IOOperation.READ);
 
 		if (io == null) {
@@ -416,6 +448,12 @@ public class GServerDirectedIO extends GAggregationCache {
 		List<IOJob> jobList = queuedWriteJobs.get(req.getFile());
 		int i = jobList.size();
 
+		/* This loop is used to determine the position to insert the new IOJob at.
+		 * Real implementations should use a more efficient approach (like a binary
+		 * search), but for now we just search the position from the end of the
+		 * job queue and modify existing IOJobs along the way.
+		 *
+		 *  We must make sure that IOJobs do not overlap and are sorted by offset. */
 		while (true) {
 			if (i == 0) {
 				jobList.add(0, io);
@@ -426,10 +464,12 @@ public class GServerDirectedIO extends GAggregationCache {
 
 			if (cmp.getOffset() >= io.getOffset()) {
 				if (io.getOffset() + io.getSize() >= cmp.getOffset() + cmp.getSize()) {
-					// new operation overwrites the old one
+					/* The new operation completely overwrites the old one, so we
+					 * can delete it. */
 					jobList.remove(i - 1);
 				} else if (io.getOffset() + io.getSize() > cmp.getOffset()) {
-					// new operation overwrites part of the old one
+					/* The new operation overwrites part of the old one, so we must
+					 * adjust the offset and size. */
 					long tmpOffset = io.getOffset() + io.getSize();
 					long tmpSize = cmp.getSize() - (tmpOffset - cmp.getOffset());
 					IOJob tmp = new IOJob(cmp.getFile(), tmpSize, tmpOffset, cmp.getType());
@@ -441,7 +481,7 @@ public class GServerDirectedIO extends GAggregationCache {
 			} else {
 				if (io.getOffset() < cmp.getOffset() + cmp.getSize()) {
 					if (io.getOffset() + io.getSize() < io.getOffset() + io.getSize()) {
-						// new operation splits up the old one
+						/* The new operation splits the old one into two. */
 						long upSize = (cmp.getOffset() + cmp.getSize()) - (io.getOffset() + io.getSize());
 						long upOffset = io.getOffset() + io.getSize();
 						IOJob up = new IOJob(cmp.getFile(), upSize, upOffset, cmp.getType());
@@ -451,7 +491,8 @@ public class GServerDirectedIO extends GAggregationCache {
 						jobList.set(i - 1, down);
 						jobList.add(i, up);
 					} else {
-						// new operation overwrites part of the old one
+						/* The new operation overwrites part of the old one, so we must
+						 * adjust the offset and size. */
 						long tmpSize = io.getOffset() - cmp.getOffset();
 						IOJob tmp = new IOJob(cmp.getFile(), tmpSize, cmp.getOffset(), cmp.getType());
 
