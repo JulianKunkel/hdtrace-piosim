27d1a98eeda874415f41dbeac1f6930a54040daf
Michael Kuhn <michael.kuhn@informatik.uni-hamburg.de>
2010-03-08 14:42:52 +0000

    Remove branches.

--- branches/ResourcesUtilizationTracingLibrary/HDMPIwrapper/src/write_info.h
+++
@@ -1,8 +0,0 @@
-#ifndef WRITE_INFO_H_
-#define WRITE_INFO_H_
-
-static void writeFileInfo(const char * name, int size, gint id);
-static void writeCommInfo(MPI_Comm comm, gint comm_id);
-static void writeTypeInfo(MPI_Datatype type, gint id);
-
-#endif
