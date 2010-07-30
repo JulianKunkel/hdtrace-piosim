58f5068f3cbcfe37a5f560a8e14465b07cc85d54
Julian Kunkel <kunkel@dkrz.de>
2010-07-30 16:30:45 +0200

    Fixed wrong assert. Commenting out one which fails due to errors in libgtop.

--- ResourcesUtilizationTracingLibrary/src/tracing.c
+++ ResourcesUtilizationTracingLibrary/src/tracing.c
@@ -486,7 +486,6 @@ static void doTracingStepCPU(tracingDataStruct *tracingData) {
 	{
 		if (tracingData->sources.CPU_UTIL)
 		{
-			assert(CPUDIFF(total[i]) != 0);
 
 			valuef = (gfloat) (1.0 - (CPUDIFF(idle) / CPUDIFF(total)));
 			WRITE_FLOAT_VALUE(tracingData, valuef * 100);
@@ -497,7 +496,7 @@ static void doTracingStepCPU(tracingDataStruct *tracingData) {
 		{
 			for (int i = 0; i < tracingData->staticData.cpu_num; ++i)
 			{
-				assert(CPUDIFF(xcpu_total[i]) != 0);
+				/* assert(CPUDIFF(xcpu_total[i]) != 0); */
 
 				/* TODO: Check CPU enable state (flags) */
 				valuef = (gfloat)(1.0 - (CPUDIFF(xcpu_idle[i])
