7a0cc620ecb4d878c5815d048ea35c9325fcb69c
Julian Kunkel <kunkel@dkrz.de>
2011-06-15 16:04:42 +0200

    Convert to new root remaping interface, TODO fix bugs and transfer size.

--- PIOsimHD-Simulator/src/de/hd/pvs/piosim/simulator/program/Gather/GatherBinaryTreeMPICH2.java
+++ PIOsimHD-Simulator/src/de/hd/pvs/piosim/simulator/program/Gather/GatherBinaryTreeMPICH2.java
@@ -1,18 +1,28 @@
 package de.hd.pvs.piosim.simulator.program.Gather;
 
+import de.hd.pvs.piosim.model.program.Communicator;
 import de.hd.pvs.piosim.model.program.commands.Gather;
 import de.hd.pvs.piosim.simulator.components.ClientProcess.CommandProcessing;
 import de.hd.pvs.piosim.simulator.components.ClientProcess.GClientProcess;
-import de.hd.pvs.piosim.simulator.components.ClientProcess.ICommandProcessing;
+import de.hd.pvs.piosim.simulator.components.ClientProcess.ICommandProcessingMapped;
 import de.hd.pvs.piosim.simulator.network.NetworkJobs;
 import de.hd.pvs.piosim.simulator.network.jobs.NetworkSimpleData;
-import de.hd.pvs.piosim.simulator.program.CommandImplementation;
+import de.hd.pvs.piosim.simulator.program.CommandImplementationWithCommunicatorLocalRanksRemapRoot;
 
-public class GatherBinaryTreeMPICH2 extends CommandImplementation<Gather>{
+public class GatherBinaryTreeMPICH2 extends CommandImplementationWithCommunicatorLocalRanksRemapRoot<Gather>{
+
 
 	@Override
-	public void process(Gather cmd, ICommandProcessing OUTResults,
-			GClientProcess client, long step, NetworkJobs compNetJobs) {
+	public int getSingleTargetWorldRank(Gather cmd) {
+		return cmd.getRootRank();
+	}
+
+	@Override
+	public void processWithLocalRanks(Gather cmd,
+		ICommandProcessingMapped OUTResults, Communicator comm,
+		int clientRankInComm, int rootRank, GClientProcess client,
+		long step, NetworkJobs compNetJobs)
+	{
 
 		if (cmd.getCommunicator().getSize() == 1){
 			// finished ...
@@ -20,60 +30,52 @@ public class GatherBinaryTreeMPICH2 extends CommandImplementation<Gather>{
 		}
 
 		final int commSize = cmd.getCommunicator().getSize();
-		final int myRank = client.getModelComponent().getRank();
-		final int rootRank = cmd.getRootRank();
 		final int pof2Offset = commSize - pof2(commSize);
 		final int iterations = Integer.numberOfTrailingZeros(commSize);
 
-		int clientRankInComm = myRank;
-
-		//exchange rank 0 with cmd.root to receive data on the correct node
-		if(clientRankInComm == cmd.getRootRank()) {
-			clientRankInComm = 0;
-		}else if(clientRankInComm == 0) {
-			clientRankInComm = rootRank;
-		}
-
-
-
-		if(myRank == rootRank){
+		if(clientRankInComm == 0){
 			// calculate from whom to receive
 			int receiveFrom = (int) Math.pow(2, step);
 
 			// check if receiveFrom is greater than the number of nodes
 			// if not => receive data
 			if(receiveFrom < commSize){
-				OUTResults.addNetReceive(receiveFrom, 30303, cmd.getCommunicator());
-				OUTResults.setNextStep(step++);
+				OUTResults.addNetReceive(getLocalRankExchangeRoot(rootRank, receiveFrom) , 30303, cmd.getCommunicator());
+				OUTResults.setNextStep(++step);
 			}else{
 				// no one to receive from
 				OUTResults.setNextStep(CommandProcessing.STEP_COMPLETED);
 			}
 			// finalize
 		}else{
-			// who will be next receiver? => (myrank - 2^step)
-			int sendTo = (int) (clientRankInComm - Math.pow(2, step));
-			int receiveFrom = (int) (clientRankInComm + Math.pow(2, step/-1));
-
-			// am i leaf node?
-			if(step == CommandProcessing.STEP_START && Integer.numberOfTrailingZeros(clientRankInComm) == 0){
-				// i am a leaf node so send data to direct neighbor
-				OUTResults.addNetSend(sendTo, new NetworkSimpleData(cmd.getSize() + 20), 30303, cmd.getCommunicator());
-				OUTResults.setNextStep(step++);
-			}else{
-				// i am not a leaf node
-
-				// receive data
-				OUTResults.addNetReceive(receiveFrom, 30303, cmd.getCommunicator());
-				// thus I send my data + the data i received to the next node (myrank - 2^step)
-				// unless this receiver is out of range (< 0)
-				if(sendTo >= 0){
-					OUTResults.addNetSend(sendTo, new NetworkSimpleData(cmd.getSize() * (int)Math.pow(2, step) + 20), 30303, cmd.getCommunicator());
-					OUTResults.setNextStep(step++);
+			if(isValuePofTwo(clientRankInComm)){
+				// I am the one who has to send data to root when the time is right (2^step == myRank)
+				if (Math.pow(2, step) == clientRankInComm){
+					// My turn to send data to root
+					OUTResults.addNetSend(getLocalRankExchangeRoot(rootRank, 0), new NetworkSimpleData(cmd.getSize() + 20), 30303, cmd.getCommunicator());
+					// finalize
+					OUTResults.setNextStep(CommandProcessing.STEP_COMPLETED);
 				}else{
+					// Not my turn to send to root
+					if(clientRankInComm < cmd.getCommunicator().getSize()-1){
+						// Receive some data first
+						if(clientRankInComm/2 >= step && cmd.getCommunicator().getSize()-1 >= (clientRankInComm + Math.pow(2, step))){
+							int receiveFrom = (int) (clientRankInComm + Math.pow(2, step));
+							OUTResults.addNetReceive(getLocalRankExchangeRoot(rootRank, receiveFrom), 30303, cmd.getCommunicator());
+							OUTResults.setNextStep(++step);
+						}
+					}
+				}
+			}else{
+				if(step == CommandProcessing.STEP_START && (clientRankInComm%2 == 0)){
+					// Receive from neighbour
+					OUTResults.addNetReceive(getLocalRankExchangeRoot(rootRank, clientRankInComm + 1), 30303, cmd.getCommunicator());
+					OUTResults.setNextStep(++step);
+				}else if(step <= Math.sqrt(clientRankInComm)){
+					int sendTo = (int)(clientRankInComm - Math.pow(2, step));
+					OUTResults.addNetSend(getLocalRankExchangeRoot(rootRank, sendTo), new NetworkSimpleData(cmd.getSize() + 20), 30303, cmd.getCommunicator());
 					OUTResults.setNextStep(CommandProcessing.STEP_COMPLETED);
 				}
-
 			}
 		}
 	}
@@ -94,6 +96,19 @@ public class GatherBinaryTreeMPICH2 extends CommandImplementation<Gather>{
 		}
 
 		return temp/2;
+	}
+
+	private boolean isValuePofTwo(int value){
+		int temp = 1;
+
+		while (temp < value){
+			temp *= 2;
+		}
+
+		if(temp == value)
+			return true;
+		else
+			return false;
 	}
 
 }
