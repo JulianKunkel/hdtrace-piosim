fe659e16d774adafdf9fd2d2a8a59361ba123ff0
Artur <artur@artur-1101HA.(none)>
2011-06-25 17:47:18 +0200

    Gather complete fixed dataMultiplicator

--- PIOsimHD-Simulator/src/de/hd/pvs/piosim/simulator/program/Gather/GatherBinaryTreeMPICH2.java
+++ PIOsimHD-Simulator/src/de/hd/pvs/piosim/simulator/program/Gather/GatherBinaryTreeMPICH2.java
@@ -31,16 +31,16 @@ public class GatherBinaryTreeMPICH2 extends CommandImplementationWithCommunicato
 
 		final int commSize = cmd.getCommunicator().getSize() -1;
 		int receiveFrom = -1, sendTo = -1;
-		int dataMultiplicator;
+		int dataMultiplicator = 1;
 
 		if(clientRankInComm == 0){
 			// calculate from whom to receive
-			receiveFrom = (int) Math.pow(2, step);
+			receiveFrom = 1<<step;
 
 			// check if receiveFrom is greater than the number of nodes
 			// if not => receive data
 			if(receiveFrom <= commSize){
-				System.out.println("Rank: " + clientRankInComm + " sendTo: " + sendTo + " receiveFrom: " + receiveFrom + " Step: " +  step);
+				System.out.println("Step: " + step + " # " + "root <- " + receiveFrom);
 				OUTResults.addNetReceive(getLocalRankExchangeRoot(rootRank, receiveFrom) , 30303, cmd.getCommunicator());
 				OUTResults.setNextStep(++step);
 			}else{
@@ -50,14 +50,12 @@ public class GatherBinaryTreeMPICH2 extends CommandImplementationWithCommunicato
 		}else{
 			if(Integer.bitCount(clientRankInComm) == 1){
 				// I am the one who has to send data to root when the time is right (2^step == myRank)
-				if (Math.pow(2, step) == clientRankInComm){
+				if ((1<<step) == clientRankInComm){
 					// My turn to send data to root
 
-					// correct the amount of data
-					dataMultiplicator = (clientRankInComm*2 <= commSize) ? clientRankInComm : commSize - clientRankInComm;
+					dataMultiplicator = (((clientRankInComm << 1) <= commSize) ? clientRankInComm : (commSize - clientRankInComm + 1));
 
-
-					System.out.println("Rank: " + clientRankInComm + " sendTo: " + sendTo + " receiveFrom: " + receiveFrom + " Step: " +  step);
+					System.out.println("Step: " + step + " # " + clientRankInComm + " -> root" + " # dm = " + dataMultiplicator);
 					OUTResults.addNetSend(getLocalRankExchangeRoot(rootRank, 0), new NetworkSimpleData(cmd.getSize() * dataMultiplicator + 20), 30303, cmd.getCommunicator());
 					// finalize
 					OUTResults.setNextStep(CommandProcessing.STEP_COMPLETED);
@@ -66,43 +64,40 @@ public class GatherBinaryTreeMPICH2 extends CommandImplementationWithCommunicato
 					// so receive data from higher ranks instead
 
 					// receive from direct neighbour first and then from even ranks only
-					receiveFrom = (int) (clientRankInComm + ((step == 0) ? 1 : 2*step));
+					receiveFrom = (int) (clientRankInComm + (1<<step));
 
 					if(receiveFrom <= commSize){
-						System.out.println("Rank: " + clientRankInComm + " sendTo: " + sendTo + " receiveFrom: " + receiveFrom + " Step: " +  step);
+						System.out.println("Step: " + step + " # " + clientRankInComm + " <- " + receiveFrom);
 						OUTResults.addNetReceive(getLocalRankExchangeRoot(rootRank, receiveFrom), 30303, cmd.getCommunicator());
 					}
 					OUTResults.setNextStep(++step);
 				}
 			}else{
 				if(clientRankInComm%2 == 0){
-					if(step == CommandProcessing.STEP_START){
-						receiveFrom = (int)(clientRankInComm + Math.pow(2, step));
+					// im am an even rank but not Po2
+					if(Integer.bitCount(clientRankInComm - (1<<step)) == 1 ){
+						// my turn to send to Po2 rank
+						sendTo = (int) (clientRankInComm - (1<<step));
+
+						int leftPo2 = 1 << (Integer.numberOfLeadingZeros(0) - Integer.numberOfLeadingZeros(clientRankInComm) - 1);
+						int rightPo2 = leftPo2 << 1;
+						dataMultiplicator = ((rightPo2 <= commSize) ? (rightPo2 - clientRankInComm) : (commSize - clientRankInComm + 1));
+						System.out.println("Step: " + step + " # " + clientRankInComm + " -> " + sendTo + " # dm = " + dataMultiplicator);
+						OUTResults.addNetSend(getLocalRankExchangeRoot(rootRank, sendTo), new NetworkSimpleData(cmd.getSize() * dataMultiplicator + 20), 30303, cmd.getCommunicator());
+						OUTResults.setNextStep(CommandProcessing.STEP_COMPLETED);
+					}else{
+						// my turn to receive from rank + 1<<step
+						receiveFrom = (int)(clientRankInComm + (1<<step));
 						if(receiveFrom <= commSize){
-							System.out.println("Rank: " + clientRankInComm + " sendTo: " + sendTo + " receiveFrom: " + receiveFrom + " Step: " +  step);
+							System.out.println("Step: " + step + " # " + clientRankInComm + " <- " + receiveFrom);
 							OUTResults.addNetReceive(getLocalRankExchangeRoot(rootRank, receiveFrom), 30303, cmd.getCommunicator());
 						}
 						OUTResults.setNextStep(++step);
-					}else{
-						sendTo = (int)(clientRankInComm - 2*step);
-						if(Integer.bitCount(sendTo) == 1){
-							System.out.println("Rank: " + clientRankInComm + " sendTo: " + sendTo + " receiveFrom: " + receiveFrom + " Step: " +  step);
-
-							// correct amount of data
-							if (clientRankInComm + 2 == commSize)
-								dataMultiplicator = 3;
-							else
-								dataMultiplicator = 2;
-
-							OUTResults.addNetSend(getLocalRankExchangeRoot(rootRank, sendTo), new NetworkSimpleData(cmd.getSize() * dataMultiplicator + 20), 30303, cmd.getCommunicator());
-							OUTResults.setNextStep(CommandProcessing.STEP_COMPLETED);
-						}else{
-							OUTResults.setNextStep(++step);
-						}
 					}
 				}else if(clientRankInComm % 2 != 0 && step == CommandProcessing.STEP_START){
+					// odd ranks send to even left neighbour (rank - 1)
 					sendTo = (int)(clientRankInComm - 1);
-					System.out.println("Rank: " + clientRankInComm + " sendTo: " + sendTo + " receiveFrom: " + receiveFrom + " Step: " +  step);
+					System.out.println("Step: " + step + " # " + clientRankInComm + " -> " + sendTo);
 					OUTResults.addNetSend(getLocalRankExchangeRoot(rootRank, sendTo), new NetworkSimpleData(cmd.getSize() + 20), 30303, cmd.getCommunicator());
 					OUTResults.setNextStep(CommandProcessing.STEP_COMPLETED);
 				}
@@ -110,3 +105,4 @@ public class GatherBinaryTreeMPICH2 extends CommandImplementationWithCommunicato
 		}
 	}
 }
+
