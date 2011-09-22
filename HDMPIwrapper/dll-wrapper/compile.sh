35ad81ef836655b61d635699fa63721e843a4557
Julian Martin Kunkel <Julian.Kunkel@gmx.de>
2011-09-22 13:44:58 +0200

    Fix

--- HDMPIwrapper/dll-wrapper/compile.sh
+++ HDMPIwrapper/dll-wrapper/compile.sh
@@ -8,7 +8,8 @@ gcc test.c -ggdb -Wall wrapper.o -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_L
 
 # COMPILE WITH NETCDF
 echo "Compiling with mpicc"
-mpicc -c -ggdb -I/opt/netcdf/4.1.1/include/ -DNETCDFLIB='"/opt/netcdf/4.1.1/lib/libnetcdf.so.4.0.0"' -I /opt/hdtrace/1.0/wrapper-plain/include/  -I /opt/hdtrace/1.0/HDTraceWritingCLibrary/include/ -Wall ./wrapper.c -o wrapper-hdtracempi.o -D HDTRACE  -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE  -ldl `pkg-config --libs glib-2.0 --cflags glib-2.0` || exit 1
+mpicc -c -ggdb -I/opt/hdf5/1.8.7-mpich2-1.4.1p1/include -I/opt/netcdf/4.1.3-hdf5-1.8.7-mpich2-1.4.1p1/include/ -DHDF5LIB='\"/opt/hdf5/1.8.7-mpich2-1.4.1p1/lib/libhdf5.so.7.0.1\"' -DNETCDFLIB='"/opt/netcdf/4.1.3-hdf5-1.8.7-mpich2-1.4.1p1/lib/libnetcdf.so.4.0.0"' -I /opt/hdtrace/1.0/wrapper/include/  -I /opt/hdtrace/1.0/HDTraceWritingCLibrary/include/ -Wall ./wrapper.c -o wrapper-hdtracempi.o -D HDTRACE  -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE  -ldl `pkg-config --libs glib-2.0 --cflags glib-2.0` || exit 1
+#mpicc -c -ggdb -I/opt/netcdf/4.1.1/include/ -DNETCDFLIB='"/opt/netcdf/4.1.1/lib/libnetcdf.so.4.0.0"' -I /opt/hdtrace/1.0/wrapper/include/  -I /opt/hdtrace/1.0/HDTraceWritingCLibrary/include/ -Wall ./wrapper.c -o wrapper-hdtracempi.o -D HDTRACE  -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE  -ldl `pkg-config --libs glib-2.0 --cflags glib-2.0` || exit 1
 #mpicc -c -ggdb -DNETCDFLIB=/opt/netcdf/4.1.1/lib/libnetcdf.so.4.0.0 -I /opt/hdtrace/1.0/wrapper/include/  -I /opt/hdtrace/1.0/HDTraceWritingCLibrary/include/ -Wall ./wrapper.c -o wrapper-hdtracempi.o -D HDTRACE  -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE  -ldl `pkg-config --libs glib-2.0 --cflags glib-2.0`
 
 
