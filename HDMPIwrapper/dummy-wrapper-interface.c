4bc02fd0eb5e51a75a5d9448befcfd725308efdf
Julian Martin Kunkel <Julian.Kunkel@gmx.de>
2011-10-04 18:29:25 +0200

    Dummy interfaces.

---
+++ HDMPIwrapper/dummy-wrapper-interface.c
@@ -0,0 +1,16 @@
+void hdMPI_threadLogStateEnd(){
+}
+
+void hdMPI_threadLogStateStart(){
+}
+void hdMPI_logInternalsMPI_Send(){
+}
+void hdMPI_logInternalsMPI_Recv(){
+}
+void hdMPI_logInternalsMPI_Isend(){
+}
+void hdMPI_logInternalsMPI_Irecv(){
+}
+void hdMPI_logInternalsMPI_Sendrecv(){
+}
+
