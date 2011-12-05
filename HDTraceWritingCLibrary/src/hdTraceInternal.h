5ec607363549bd114fe46c1bd9cda442d257b906
Nathanael Hübbe <nathanael.huebbe@informatik.uni-hamburg.de>
2011-12-05 15:51:53 +0100

    Phased out some fixed size buffers. Warning: Also removed the corresponding preprocessor constants. Since they were declared in a public header, this might break other code. If it does: These limits are no longer valid for the HDTraceWritingLibrary, so the broken code should be changed accordingly.

--- HDTraceWritingCLibrary/src/hdTraceInternal.h
+++ HDTraceWritingCLibrary/src/hdTraceInternal.h
@@ -2,88 +2,38 @@
  * @file hdTraceInternal.h
  *
  * @date 15.07.2009
- * @author Stephan Krempel <stephan.krempel@gmx.de>
- * @version 0.1
+ * @author Stephan Krempel <stephan.krempel@gmx.de> (original version)
+ * @author 2011: Nathanael Hübbe (Phased out a number of fixed length arrays and split the implementation into two classes.)
+ * @version 0.2
  */
 
 #ifndef HDTRACE_INTERNAL_H_
 #define HDTRACE_INTERNAL_H_
+
+typedef struct _hdTraceState hdTraceState;
 
 /**
  * @internal
  * Structure representing one trace
  */
 struct _hdTrace {
-	/**
-	 * File descriptor of the XML file
-	 */
-	int log_fd;
+	int log_fd;	///< File descriptor of the XML file
+	int info_fd;	///< File descriptor for info file
+	struct timeval init_time;	///< the initial time when the trace file was opened
+	char *logfile;	///< Name of the log file (for error output)
+	char *infofile;	///< Name of the info file (for error output)
+	hdTopoNode *topoNode;	///< Topology leaf this trace belongs to.
 
-	/**
-	 * File descriptor for info file
-	 */
-	int info_fd;
+	int isEnabled;	///< Selects whether tracing is enabled, can be set with \a hdT_enableTrace() and \a hdT_disableTrace().
+	int always_flush;	///< Switches off the use of the buffer to force every write operation to be written directly to disk.
 
-	/**
-     * the initial time when the trace file was opened
-     */
-	struct timeval init_time;
-
-	/**
-	 * Name of the log file (for error output)
-	 */
-	char *logfile;
-
-	/**
-	 * Name of the info file (for error output)
-	 */
-	char *infofile;
-
-	/**
-	 * This array of strings is used to save the name of the
-	 * last state that has been started via \a hdT_logStateStart(...)
-	 * in a certain level.
-	 * For example, calling
-	 * \code
-	 * hdT_logStateStart(trace, "A");
-	 * hdT_logStateStart(trace, "B");
-	 * hdT_logStateEnd(trace);
-	 * hdT_logStateEnd(trace);
-	 * \endcode
-	 * will lead to
-	 * \code
-	 * state_name[0] = "A"
-	 * state_name[1] = "B"
-	 * \endcode
-	 */
-	char state_name[HD_LOG_MAX_DEPTH][HD_LOG_ELEMENT_NAME_BUF_SIZE];
-
-	/**
-	 * This array of strings is used to buffer the attribute
-	 * string for the corresponding level. That means,
-	 * \a attributes[i] holds the attribute string for the
-	 * i-th level of nesting.
-	 */
-	char attributes[HD_LOG_MAX_DEPTH][HD_LOG_COMMAND_BUF_SIZE];
-
-	/**
-	 * This array holds the position, at which the writing has
-	 * stopped in tha attributes string with the corresponding level.
-	 */
-	size_t attributes_pos[HD_LOG_MAX_DEPTH];
-
-	/**
-	 * This array of strings holds the element string that has
-	 * been written via \a hdT_logElement(...).
-	 * \a elements[i] holds the string for the i-th level
-	 * of nesting
-	 */
-	char elements[HD_LOG_MAX_DEPTH][HD_LOG_COMMAND_BUF_SIZE];
-
-	/**
-	 * \a elements_pos[i] holds the write position for \a elements[i]
-	 */
-	size_t elements_pos[HD_LOG_MAX_DEPTH];
+	long max_nesting_depth;	///< The maximal nesting level to which calls are recorded. Calls exeeding this limit will not show up in the trace.
+	long function_depth;	///< Keeps track of the depth of nested function calls
+	hdTraceState** states;	///< This state array keeps track of all the nested calls encountered (has up to max_nesting_depth entries). The current state is always states[function_depth] (iff function_depth is not negative).
+	long statesSize;	///< The current size of the states array in elements. Some may be NULL. Also controls the size of the whitespace string.
+	char* tabString;	///< The string to be used for indentation, default is "\t".
+	long tabStringLength;	///< strlen() of tabString.
+	char* whitespaceString;	///< statesSize+1 copies of tabString concatenated. Thus, for any function_depth, the correct indentation string can be found at &whitespaceString[(statesSize+1 - function_depth)*tabStringLength], or, even simpler, by calling hdT_getIndentationString().
 
 	/**
 	 * This is the write buffer of the trace file. Data is not written
@@ -97,60 +47,54 @@ struct _hdTrace {
 	 * can occur on \a buffer.
 	 */
 	size_t buffer_pos;
-
-	/**
-	 * \a start_time[i] holds the start time of the last logged tate in
-	 * the i-th nesting level.
-	 */
-	struct timeval start_time[HD_LOG_MAX_DEPTH];
-
-	/**
-	 * \a end_time[i] holds the ending time of the last
-	 * logged state in the i-th nesting level.
-	 */
-	struct timeval end_time[HD_LOG_MAX_DEPTH];
-
-	/**
-	 * Keeps track of the depth of nested function calls
-	 */
-	int function_depth;
-
-	/**
-	 * Topology leaf this trace belongs to.
-	 */
-	hdTopoNode *topoNode;
-
-	/**
-	 * \a isEnabled = 1 if the trace is enabled.
-	 * \a isEnabled = 0 if the trace is desabled.
-	 *
-	 * Enabling and disabling can be done via
-	 * \a hdT_enableTrace(...) and \a hdT_disableTrace(...)
-	 */
-	int isEnabled;
-
-	/**
-	 * if \a always_flush = 0,  \a buffer is used to cache
-	 * write operations on the trace file. Otherwise, every write
-	 * operation is performed via the \a write function
-	 */
-	int always_flush;
-
-	/**
-	 * 
-	 */
-	int max_nesting_depth;
-
-    /**
-     * \a has_nested[i] = 1 if functions with depth i+1 have been logged.
-	 * otherwise 0. The variable is reset after \a hdT_logStateEnd is called with
-	 * \a function_depth == i.
-	 *
-	 * In other words, \a has_nested[i] == 1 if and only if the
-	 * state with nesting level \a i has nested operations.
-	 */
-	int has_nested[HD_LOG_MAX_DEPTH];
 };
 
+/// sprintf like function writing to trace log instead of string.
+static int writeLogf(hdTrace *trace, const char * format, ...) __attribute__ ((format (printf, 2, 3)));
+
+/// vsprintf like function writing to trace log instead of string.
+static int writeLogfv(hdTrace *trace, const char * format, va_list valist);
+
+/// Write a message to the trace log buffer and flush to file if needed.
+static int writeLog(hdTrace *trace, const char * message);
+
+static int flushLog(hdTrace *trace);	///< Flush the buffer of trace log.
+static int writeState(hdTrace *trace);	///< Write state to trace log.
+static char* hdT_getIndentationString(hdTrace *trace, int count);	///< Get a string containing count concatenated copies of tabString.
+
+////////////////////////////////////
+
+/**
+ * @internal
+ * Structure to accumulate the infos for a traced call (=state).
+ */
+struct _hdTraceState {
+	char* name;	///< The name of the state.
+	char* attributes;	///< The attributes string. These are included in the opening tag of the state.
+	char* elements;	///< The elements string. These are included between the opening and closing tags for the state.
+	struct timeval start_time, end_time;	///< The timing information that will be appended to the attribute list.
+	int hasNested;	///< True if there have been nested calls in this state.
+
+	long nameLength, attributesLength, elementsLength;	///< The number of chars in each string, excluding the termination.
+	long nameSize, attributesSize, elementsSize;	///< The amount of space allocated for each string.
+};
+
+static hdTraceState* hdTS_create();
+static void hdTS_construct(hdTraceState* self);
+
+static int hdTS_start(hdTraceState* self, const char* name);
+static void hdTS_setNested(hdTraceState* self);
+static int hdTS_getNested(hdTraceState* self);
+static int hdTS_end(hdTraceState* self);
+static char* hdTS_createTag(hdTraceState* self, const char* indentation, long offsetSeconds);	///< The caller is responsible to free the return value.
+
+static int hdTS_ensureNameSpace(hdTraceState* self, long lengthRequest);
+static int hdTS_ensureAttributesSpace(hdTraceState* self, long lengthRequest);
+static int hdTS_ensureElementsSpace(hdTraceState* self, long lengthRequest);
+static int hdTS_appendAttributes(hdTraceState* self, const char* someAttributes);
+static int hdTS_appendElements(hdTraceState* self, const char* someElements);
+
+static void hdTS_destruct(hdTraceState* self);
+static void hdTS_delete(hdTraceState* self);
 
 #endif /* HDTRACE_INTERNAL_H_ */
