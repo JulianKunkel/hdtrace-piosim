969d4e55fc0c580e4857da7cf7afc41ee8d48627
Julian Kunkel <juliankunkel@googlemail.com>
2014-06-05 13:04:41 +0200

    Updated benchmark with the correct version from 2012.

--- PIOsimHD-Maint/benchmarks/mpi-bench.c
+++ PIOsimHD-Maint/benchmarks/mpi-bench.c
@@ -7,6 +7,7 @@
 #include <mpi.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <netdb.h>
 
 
@@ -19,16 +20,16 @@ static Options options = {3};
 static int rank, nproc;
 
 /* returns the test time */
-typedef double (*testFunc)(long param1, double * totalTime);
+typedef double (*testFunc)(int param1, double * totalTime);
 
 
 typedef struct{
    const testFunc func;
    char const * name;
-   long param1;
+   int param1;
 }Test;
 
-static Test * currentTest = NULL;
+static const Test * currentTest = NULL;
 
 #define getTime MPI_Wtime
 
@@ -37,22 +38,21 @@ static Test * currentTest = NULL;
 #define STARTTIME   MPI_Barrier(MPI_COMM_WORLD);  double t = getTime(); 
 #define GETTIME   double e = getTime() - t;  MPI_Barrier(MPI_COMM_WORLD); *totalTime = getTime() -t;
 
-void * mallocWithCheck(long size){
+void * mallocWithCheck(long long size){
   void * ret = malloc(size);
   if (ret == NULL){
-    printf("Error could not malloc %lld of bytes\n", (long long int) size);
-    printf("Current test %s with param %ld\n", currentTest->name, currentTest->param1);
+    printf("Error could not malloc %lld of bytes\n", (long long) size);
+    printf("Current test %s with param %d\n", currentTest->name, currentTest->param1);
     MPI_Abort(1, MPI_COMM_WORLD);
   }
   memset(ret, 0, size);
+
   return ret;
 }
 
-double reduceTo0(long size, double * totalTime){
-  double * sendbuf = mallocWithCheck(size*8);
-  double * recvbuf = mallocWithCheck(size*8);
-  
-  memset(sendbuf, 0, size*8);
+double reduceTo0(int size, double * totalTime){
+  double * sendbuf = mallocWithCheck((long long) size*8);
+  double * recvbuf = mallocWithCheck((long long) size*8);
   
   STARTTIME
   int ret = MPI_Reduce(sendbuf, recvbuf, size, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
@@ -66,11 +66,9 @@ double reduceTo0(long size, double * totalTime){
   return e;
 }
 
-double allreduce(long size, double * totalTime){
-  double * sendbuf = mallocWithCheck(size*8);
-  double * recvbuf = mallocWithCheck(size*8);
-  
-  memset(sendbuf, 0, size*8);
+double allreduce(int size, double * totalTime){
+  double * sendbuf = mallocWithCheck((long long) size*8);
+  double * recvbuf = mallocWithCheck((long long) size*8);
   
   STARTTIME
   int ret = MPI_Allreduce(sendbuf, recvbuf, size, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
@@ -84,10 +82,8 @@ double allreduce(long size, double * totalTime){
 }
 
 
-double bcast(long size, double * totalTime){
-  double * buffer = mallocWithCheck(size*8);
-  
-  memset(buffer, 0, size*8);
+double bcast(int size, double * totalTime){
+  double * buffer = mallocWithCheck((long long) size*8);
   
   STARTTIME  
   int ret = MPI_Bcast(buffer, size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
@@ -99,21 +95,20 @@ double bcast(long size, double * totalTime){
 }
 
 
-double barrier(long unused, double * totalTime){
+double barrier(int unused, double * totalTime){
  
   STARTTIME
-  int ret = MPI_Barrier(MPI_COMM_WORLD);
-  CHECKRET
-  GETTIME
+  MPI_Barrier(MPI_COMM_WORLD);
+  double e = getTime() - t;  
+  *totalTime = e;
+
   return e;
 }
 
 
-double gather(long size, double * totalTime){
-  double * sendbuf = mallocWithCheck(size*8);
-  double * recvbuf = mallocWithCheck(size*8 * nproc);
-  
-  memset(sendbuf, 0, size*8);
+double gather(int size, double * totalTime){
+  double * sendbuf = mallocWithCheck((long long) size*8);
+  double * recvbuf = mallocWithCheck((long long) size*8 * nproc);
   
   STARTTIME
   int ret =     MPI_Gather(sendbuf, size, MPI_DOUBLE, recvbuf, size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
@@ -125,11 +120,9 @@ double gather(long size, double * totalTime){
   return e;
 }
 
-double allgather(long size, double * totalTime){
-  double * sendbuf = mallocWithCheck(size*8);
-  double * recvbuf = mallocWithCheck(size*8 * nproc);
-  
-  memset(sendbuf, 0, size*8);
+double allgather(int size, double * totalTime){
+  double * sendbuf = mallocWithCheck((long long) size*8);
+  double * recvbuf = mallocWithCheck((long long) size*8 * nproc);
   
   STARTTIME
   int ret =     MPI_Allgather(sendbuf, size, MPI_DOUBLE, recvbuf, size, MPI_DOUBLE, MPI_COMM_WORLD);
@@ -143,11 +136,9 @@ double allgather(long size, double * totalTime){
 
 
 
-double scatter(long size, double * totalTime){
-  double * sendbuf = mallocWithCheck(size*8 * nproc);
-  double * recvbuf = mallocWithCheck(size*8);
-  
-  memset(sendbuf, 0, size*8);
+double scatter(int size, double * totalTime){
+  double * sendbuf = mallocWithCheck((long long) size*8 * nproc);
+  double * recvbuf = mallocWithCheck((long long) size*8);
   
   STARTTIME
   int ret =     MPI_Scatter(sendbuf, size, MPI_DOUBLE, recvbuf, size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
@@ -160,11 +151,9 @@ double scatter(long size, double * totalTime){
 }
 
 
-double sendRecvRightNeighbour(long size, double * totalTime){
-  double * sendbuf = mallocWithCheck(size*8);
-  double * recvbuf = mallocWithCheck(size*8);
-  
-  memset(sendbuf, 0, size*8);
+double sendRecvRightNeighbour(int size, double * totalTime){
+  double * sendbuf = mallocWithCheck((long long) size*8);
+  double * recvbuf = mallocWithCheck((long long) size*8);
   
   const int dest = rank + 1 == nproc ? 0 : rank + 1 ;
   const int source = rank - 1 >= 0 ? rank - 1 : nproc -1;
@@ -181,17 +170,15 @@ double sendRecvRightNeighbour(long size, double * totalTime){
 }
 
 
-double sendRecvPaired(long size, double * totalTime){
+double sendRecvPaired(int size, double * totalTime){
   
   if(nproc % 2 == 1){
     /* test is run only with equal number of processes */
     return 0;
   }
   
-  double * sendbuf = mallocWithCheck(size*8);
-  double * recvbuf = mallocWithCheck(size*8);
-  
-  memset(sendbuf, 0, size*8);
+  double * sendbuf = mallocWithCheck((long long) size*8);
+  double * recvbuf = mallocWithCheck((long long) size*8);
   
   
   const int dest = rank % 2 == 0 ? rank + 1 : rank -1;
@@ -209,14 +196,13 @@ double sendRecvPaired(long size, double * totalTime){
 
 
 
-double sendToRoot(long size, double * totalTime){
-  double * sendbuf = mallocWithCheck(size*8);
-  double * recvbuf = mallocWithCheck(size*8);
-
-  memset(sendbuf, 0, size*8);
+double sendToRoot(int size, double * totalTime){
+  double * sendbuf = mallocWithCheck((long long) size*8);
+  double * recvbuf = mallocWithCheck((long long) size*8);
 
   STARTTIME
   if(rank == 0){
+    printf("Size: %d \n", size);
     for(int i=1; i < nproc; i++){
       int ret = MPI_Recv( sendbuf, size, MPI_DOUBLE, i, 4711, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
       CHECKRET
@@ -233,11 +219,9 @@ double sendToRoot(long size, double * totalTime){
   return e;
 }
 
-double sendRecvToRoot(long size, double * totalTime){
-  double * sendbuf = mallocWithCheck(size*8);
-  double * recvbuf = mallocWithCheck(size*8);
-
-  memset(sendbuf, 0, size*8);
+double sendRecvToRoot(int size, double * totalTime){
+  double * sendbuf = mallocWithCheck((long long) size*8);
+  double * recvbuf = mallocWithCheck((long long) size*8);
 
   STARTTIME
   if(rank == 0){
@@ -263,23 +247,24 @@ const Test tests[] = {
 {reduceTo0, "Reduce10K", 10*1024 / 8}, 
 {reduceTo0, "Reduce1M", 1024*1024 / 8}, 
 {reduceTo0, "Reduce10M", 10*1024*1024 / 8}, 
-{reduceTo0, "Reduce100M", 100*1024*1024 / 8}, 
 #ifdef GByte
-{reduceTo0, "Reduce1000M", 1000*1024*1024 / 8},
+{reduceTo0, "Reduce100M", 100*1024*1024 / 8}, 
+{reduceTo0, "Reduce1000M", 1000llu*1024*1024 / 8},
 #endif
 {allreduce, "Allreduce10K", 10*1024 / 8}, 
 {allreduce, "Allreduce1M",  1024*1024 / 8}, 
 {allreduce, "Allreduce10M",  10*1024*1024 / 8}, 
-{allreduce, "Allreduce100M", 100*1024*1024 / 8}, 
 #ifdef GByte
-{allreduce, "Allreduce1000M", 1000*1024*1024 / 8},
+{allreduce, "Allreduce100M", 100*1024*1024 / 8}, 
+{allreduce, "Allreduce1000M", 1000llu*1024*1024 / 8},
 #endif
 {bcast, "Broadcast10K", 10*1024 / 8},
 {bcast, "Broadcast1M",   1*1024*1024 / 8},
 {bcast, "Broadcast10M",   10*1024*1024 / 8},
 {bcast, "Broadcast100M", 100*1024*1024 / 8},
 #ifdef GByte
-{bcast, "Broadcast1000M", 1000*1024*1024 / 8},
+{bcast, "Broadcast1000M", 1000llu*1024*1024 / 8},
+//{bcast, "Broadcast10000M", 10000llu*1024*1024 / 8},
 #endif
 
 {barrier, "Barrier\t", 0},
@@ -289,17 +274,24 @@ const Test tests[] = {
 {allgather, "Allgather10M",   10*1024*1024 / 8},
 #ifdef GByte
 {allgather, "Allgather100M", 100*1024*1024 / 8},
+{allgather, "Allgather1000M", 1000llu*1024*1024 / 8},
 #endif
 
 {gather, "Gather10K", 10*1024 / 8},
 {gather, "Gather1M",   1*1024*1024 / 8},
 {gather, "Gather10M",   10*1024*1024 / 8},
+#ifdef GByte
 {gather, "Gather100M", 100*1024*1024 / 8},
+{gather, "Gather1000M", 1000llu*1024*1024 / 8},
+#endif
 
 {scatter, "Scatter10K", 10*1024 / 8},
 {scatter, "Scatter1M",   1*1024*1024 / 8},
 {scatter, "Scatter10M",   10*1024*1024 / 8},
+#ifdef GByte
 {scatter, "Scatter100M", 100*1024*1024 / 8},
+{scatter, "Scatter1000M", 1000llu*1024*1024 / 8},
+#endif
 
 {sendRecvRightNeighbour, "sendRecvRightNeighbour10K", 10*1024 / 8},
 {sendRecvRightNeighbour, "sendRecvRightNeighbour1M", 1*1024*1024 / 8},
@@ -307,6 +299,7 @@ const Test tests[] = {
 {sendRecvRightNeighbour, "sendRecvRightNeighbour100M", 100*1024*1024 / 8},
 #ifdef GByte
 {sendRecvRightNeighbour, "sendRecvRightNeighbour1000M", 1000*1024*1024 / 8},
+{sendRecvRightNeighbour, "sendRecvRightNeighbour10000M", 10000llu*1024*1024 / 8},
 #endif
 {sendRecvPaired, "sendRecvPaired10K", 10*1024 / 8},
 {sendRecvPaired, "sendRecvPaired1M", 1*1024*1024 / 8},
@@ -314,6 +307,7 @@ const Test tests[] = {
 {sendRecvPaired, "sendRecvPaired100M", 100*1024*1024 / 8},
 #ifdef GByte
 {sendRecvPaired, "sendRecvPaired1000M", 1000*1024*1024 / 8},
+{sendRecvPaired, "sendRecvPaired10000M", 10000llu*1024*1024 / 8},
 #endif
 {sendToRoot, "sendToRoot10K", 10*1024 / 8},
 {sendToRoot, "sendToRoot100K", 100*1024 / 8},
@@ -322,6 +316,7 @@ const Test tests[] = {
 {sendToRoot, "sendToRoot100M", 100*1024*1024 / 8},
 #ifdef GByte
 {sendToRoot, "sendToRoot1000M", 1000*1024*1024 / 8},
+//{sendToRoot, "sendToRoot10000M", 10000llu*1024*1024 / 8},
 #endif
 {sendRecvToRoot, "sendRecvToRoot10K", 10*1024 / 8},
 {sendRecvToRoot, "sendRecvToRoot100K", 100*1024 / 8},
@@ -330,6 +325,7 @@ const Test tests[] = {
 {sendRecvToRoot, "sendRecvToRoot100M", 100*1024*1024 / 8},
 #ifdef GByte
 {sendRecvToRoot, "sendRecvToRoot1000M", 1000*1024*1024 / 8},
+//{sendRecvToRoot, "sendRecvToRoot10000M", 10000llu*1024*1024 / 8},
 #endif
 {NULL, NULL, 0}
 };
@@ -388,7 +384,7 @@ int main (int argc, char *argv[])
   }
 
   MPI_Barrier(MPI_COMM_WORLD);
-  sleep(5);
+  sleep(1);
   MPI_Barrier(MPI_COMM_WORLD);
 
   for(int t = 0; t < testCount; t++){
